3bba6dc3b641d9113dd6578726fee1c5b25a8f22
ana117 <63540951+ana117@users.noreply.github.com>
2024-03-16 23:35:29 +0700

    Refactor name

---
+++ src/main/java/com/cloudcipher/cloudcipher_server/CloudCipherServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloudcipher.cloudcipher_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCipherServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCipherServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudcipher/cloudcipher_server/CloudCipherServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudcipher.cloudcipher_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCipherServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
